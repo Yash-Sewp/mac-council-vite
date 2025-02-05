c47f6f1ab7872c20a4362f0c6a18e1b3fac4ecd2
Yashveer Sewparthab <yashveersewparthab@gmail.com>
2025-02-06 00:47:22 +0200

    dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import lottie from "lottie-web";
 import mAnimation from "./animations/m-animation.json";
 import aAnimation from "./animations/a-animation.json";
 import cAnimation from "./animations/c-animation.json";
+import logo from "./assets/logo.svg"; // Import the logo image
 
 function App() {
   const mAnimationContainer = useRef(null);
@@ -11,6 +12,9 @@ function App() {
 
   const [cStartRotation, setCStartRotation] = useState(0);
   const [aStartRotation, setAStartRotation] = useState(0);
+  
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   let mAnimationInstance = null;
   let aAnimationInstance = null;
@@ -62,6 +66,10 @@ function App() {
   const playAnimations = () => {
     const playAnimation = (animationInstance, inputId, startRotation) => {
       const percentage = parseFloat(document.getElementById(inputId).value);
+
+      // title = document.getElementById('title').value;
+      // description = document.getElementById('description').value;
+
       if (animationInstance && percentage > 0) {
         const totalDegrees = 360; // Full circle
         const endRotation = startRotation + (percentage / 100) * totalDegrees;
@@ -91,49 +99,83 @@ function App() {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen w-full bg-black">
-        <div className="flex w-1/2 justify-center">
+        <div className="w-1/3 p-5">
           <div className="flex items-center relative" style={{ width: 400, height: 400 }}>
             <div id="mAnimationContainer" className="absolute" ref={mAnimationContainer} style={{ width: 400, height: 400 }}></div>
             <div id="cAnimationContainer" className="absolute" ref={cAnimationContainer} style={{ width: 400, height: 400, transform: `rotate(-${cStartRotation}deg)` }}></div>
             <div id="aAnimationContainer" className="absolute" ref={aAnimationContainer} style={{ width: 400, height: 400, transform: `rotate(-${aStartRotation}deg)` }}></div>
           </div>
+          {/* <img src={logo} alt="Logo" /> */}
+
+          <hr className="w-25 mt-4 mb-4" />
+          <h2 className="text-2xl">{title}</h2>
+          
+          <p>{description}</p>
         </div>
-        <div className="flex w-1/2 justify-center">
-          <div className="flex flex-col items-center">
-            <div className="flex">
+        <div className="w-1/2 p-5">
+          <div className="flex flex-col w-full">
+            <h1 className="text-white text-2xl mb-5 font-bold">GENERATE YOUR <br /> OWN CUSTOM LOGO</h1>
+
+            <div className="flex w-full">
+              <div className="text-white p-2">Chart Name:</div>
+              <input
+                type="text"
+                className="text-white border-b border-white p-2 mb-4 bg-transparent w-100"
+                id="title"
+                placeholder="Income"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+
+            <div className="flex mt-5 w-full">
               <div className="flex">
                 <div className="text-white p-2">Date Field M:</div>
                 <input type="number" className="text-white border border-white p-2 mb-4" id="mPercentage" placeholder="Enter percentage for M Animation" />
               </div>
-              <div className="flex">
+              <div className="flex ms-5">
                 <div className="text-white p-2">Label</div>
                 <input type="text" className="text-white border border-white p-2 mb-4" id="mLabel" placeholder="R100 to 400k" />
               </div>
             </div>
 
-            <div className="flex">
+            <div className="flex mt-5">
               <div className="flex">
                 <div className="text-white p-2">Date Field C:</div>
                 <input type="number" className="text-white border border-white p-2 mb-4" id="cPercentage" placeholder="Enter percentage for C Animation" />
               </div>
-              <div className="flex">
+              <div className="flex ms-5">
                 <div className="text-white p-2">Label</div>
                 <input type="text" className="text-white border border-white p-2 mb-4" id="cLabel" placeholder="R10000k+" />
               </div>
             </div>
 
-            <div className="flex">
+            <div className="flex mt-5">
               <div className="flex">
                 <div className="text-white p-2">Date Field A:</div>
                 <input type="number" className="text-white border border-white p-2 mb-4" id="aPercentage" placeholder="Enter percentage for A Animation" />
               </div>
-              <div className="flex">
+              <div className="flex ms-5">
                 <div className="text-white p-2">Label</div>
                 <input type="text" className="text-white border border-white p-2 mb-4" id="aLabel" placeholder="R500 to 1000k" />
               </div>
             </div>
 
-            <button className="text-white p-2 rounded" onClick={playAnimations}>Play Animations</button>
+            <div className="flex mt-5">
+              <div className="text-white p-2">Description:</div>
+              <textarea
+                className="text-white border border-white w-75 bg-transparent p-2"
+                id="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></textarea>
+            </div>
+
+            <div className="flex w-full justify-start mt-5">
+              <button className="text-white p-2 rounded mt-5 w-75" onClick={playAnimations}>Submit</button>
+              <button className="text-white p-2 rounded mt-5 w-75 ms-3">Reset</button>
+            </div>
+ 
           </div>
         </div>
       </div>
